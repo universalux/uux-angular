3bd75d2e634d67b6103b82e94405acad19b2225e
aldaydev <aldaydev@gmail.com>
2025-10-12 13:12:49 +0200

    chore: update-initial-config-for-uux-angular #1

--- projects/demo-app/src/app/app.ts
+++ projects/demo-app/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('demo-app');
+
 }
