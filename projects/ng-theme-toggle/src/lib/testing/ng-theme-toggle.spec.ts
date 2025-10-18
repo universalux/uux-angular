a40e2cf40f0b6e759ab3a44a7d34160f03a187c0
aldaydev <aldaydev@gmail.com>
2025-10-18 15:29:39 +0200

    feat: ng-theme-toggle lib and playground created + initial config + fixed test #14

--- projects/ng-theme-toggle/src/lib/testing/ng-theme-toggle.spec.ts
+++ projects/ng-theme-toggle/src/lib/testing/ng-theme-toggle.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgThemeToggle } from '../ng-theme-toggle';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 describe('NgThemeToggle', () => {
   let component: NgThemeToggle;
@@ -8,7 +9,8 @@ describe('NgThemeToggle', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NgThemeToggle]
+      imports: [NgThemeToggle],
+      providers: [provideZonelessChangeDetection()]
     })
     .compileComponents();
 
