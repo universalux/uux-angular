3c3894055a96412ab12d0f5480f8d3527be6fccb
AldayDev <aldaydev@gmail.com>
2025-10-22 19:14:19 +0200

    38 create ng hero carousel lib (#48)
    * feat: ng-hero-carousel created + playground #38
    
    * feat: ng-hero-carousel done + logic + styles + playground #38

---
+++ projects/demo-app/src/app/playgrounds/pg-hero-carousel/pg-hero-carousel.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { NgHeroCarousel, CarouselItem, SlideForDirective, AccessibilityOptions} from 'ng-hero-carousel';
+import { heroCarouselMock } from './mock/hero-carousel.mock';
+
+@Component({
+  selector: 'pg-hero-carousel',
+  imports: [NgHeroCarousel, SlideForDirective],
+  templateUrl: './pg-hero-carousel.html',
+  styleUrl: './pg-hero-carousel.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PgHeroCarousel {
+  items = signal<CarouselItem[]>(heroCarouselMock);
+
+  selectedItem = signal<number>(0);
+
+  // accOptions = signal<AccessibilityOptions>({
+  //   autoplayPauseLabel: 'Pause carousel CHANGED',
+  // })
+
+
+  accOpts = signal<AccessibilityOptions>({
+    autoplayPauseLabel: 'Pause carousel autoplay CHANGED',
+    autoplayPlayLabel: 'Resume carousel autoplay CHANGED',
+    hostAriaLabel: 'Main carousel CHANGED',
+    prevBtnAriaLabel: 'Go to previous slide CHANGED',
+    nextBtnAriaLabel: 'Go to next slide CHANGED',
+    slidesRegionAriaLabel: 'Wide carousel CHANGED',
+    slidesRegionRoleDescription: 'Carousel CHANGED',
+    slideAriaLabel: (currentSlide: number, total: number) =>
+        `CHANGED - Slide ${currentSlide} of ${total}`,
+    slideRoleDescription: 'slide',
+  });
+}
---
+++ projects/ng-hero-carousel/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-hero-carousel
+ */
+
+export * from './lib/ng-hero-carousel';
+export * from './lib/ng-hero-carousel.types';
+export * from './lib/directives/slide-for-directive';
