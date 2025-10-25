298bb46767e1122ede05e49d4bfa596ae8cfa1de
aldaydev <aldaydev@gmail.com>
2025-10-25 21:26:49 +0200

    feat: working functionalities #54

--- projects/ng-image-card/src/lib/ng-image-card.ts
+++ projects/ng-image-card/src/lib/ng-image-card.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, ChangeDetectionStrategy, Component, ContentChild, HostBinding, input, TemplateRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ContentChild, ElementRef, HostBinding, input, signal, TemplateRef, ViewChild } from '@angular/core';
 import { CardImage } from './ng-image-card.types';
 
 @Component({
@@ -10,26 +10,27 @@ import { CardImage } from './ng-image-card.types';
   styleUrl: './ng-image-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NgImageCard {
+export class NgImageCard implements AfterViewInit{
 
   @ContentChild('cardHeader') cardHeader!: TemplateRef<any>;
   @ContentChild('cardBody') cardBody!: TemplateRef<any>;
   @ContentChild('cardFooter') cardFooter!: TemplateRef<any>;
+  @ViewChild('cardImg') cardImg!: ElementRef<HTMLImageElement>;
 
   cardImage = input<CardImage | null>(null);
-  orientation = input<'vertical' | 'horizontal'>('vertical');
   imageHover = input<boolean>(true);
-  bgHover = input<boolean>(true);
-  scaleHover = input<boolean>(true);
+  bgHover = input<boolean>(false);
+  scaleHover = input<boolean>(false);
+  initialAnimation = input<boolean>(true);
 
   @HostBinding('class.scaleHover')
   get applyScaleHover() {
     return this.scaleHover();
   }
 
-  @HostBinding('class.horizontal')
-  get applyHorizontal() {
-    return this.orientation() === 'horizontal';
+  @HostBinding('class.translateAnimation')
+  get applyTranslateAnimation() {
+    return this.initialAnimation();
   }
 
   @HostBinding('class.noHeader')
@@ -50,6 +51,31 @@ export class NgImageCard {
   @HostBinding('class.noFooter')
   get noFooter(): boolean {
     return !this.cardFooter;
+  }
+
+  private _imageLoaded = signal<boolean>(false);
+
+  @HostBinding('class.loaded') get loadedClass() {
+    return this._imageLoaded();
+  }
+
+  ngAfterViewInit() {
+    if(!this.cardImage()){
+      this._imageLoaded.set(true);
+      return;
+    };
+    if (this.cardImg?.nativeElement) {
+      const img = this.cardImg.nativeElement;
+
+      if (img.complete && img.naturalHeight !== 0) {
+        // Imagen ya cacheada
+        this._imageLoaded.set(true);
+      } else {
+        img.addEventListener('load', () => {
+          this._imageLoaded.set(true);
+        });
+      }
+    }
   }
 
 }
