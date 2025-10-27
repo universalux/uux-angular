9b813ffb6779efd7108a2beb06576f3e6100246b
aldaydev <aldaydev@gmail.com>
2025-10-27 04:46:47 +0100

    feat: working on ng-content-carousel logic #32

--- projects/ng-content-carousel/src/lib/ng-content-carousel.ts
+++ projects/ng-content-carousel/src/lib/ng-content-carousel.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -8,6 +8,36 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './ng-content-carousel.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NgContentCarousel {
+export class NgContentCarousel implements AfterViewInit {
+  @ViewChild('track') track!: ElementRef;
+  currentIndex = 0;
+  cardWidth = 0;
+  transform = '';
 
+  ngAfterViewInit() {
+    console.log(this.track.nativeElement.children.length);
+    const firstCard = this.track.nativeElement.children[0];
+    if (firstCard) {
+      this.cardWidth = firstCard.offsetWidth;
+    }
+  }
+
+  next() {
+    if(this.currentIndex <= this.track.nativeElement.children.length - 1){
+      this.currentIndex++;
+      this.updateTransform();
+    }
+
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updateTransform();
+    }
+  }
+
+  updateTransform() {
+    this.transform = `translateX(-${this.currentIndex * this.cardWidth}px)`;
+  }
 }
