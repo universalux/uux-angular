e04965c410fee05770ec99349d6d782642289e80
AldayDev <aldaydev@gmail.com>
2025-11-02 20:07:50 +0100

    fix: revert deleting spc on demo-app #93 (#94)
    Fixed

---
+++ projects/demo-app/src/app/app.spec.ts
@@ -0,0 +1,20 @@
+import { provideZonelessChangeDetection } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { App } from './app';
+import { provideRouter } from '@angular/router';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [provideZonelessChangeDetection(), provideRouter([])],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+});
