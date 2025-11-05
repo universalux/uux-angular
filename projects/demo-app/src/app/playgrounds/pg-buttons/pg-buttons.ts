18e8dd170a90c4ec017e159cbde420191f0fd37f
aldaydev <aldaydev@gmail.com>
2025-11-05 08:24:25 +0100

    feat: ng-simple-button logic done #85

--- projects/demo-app/src/app/playgrounds/pg-buttons/pg-buttons.ts
+++ projects/demo-app/src/app/playgrounds/pg-buttons/pg-buttons.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { NgLinkButton } from 'ng-link-button';
 import {NgSimpleButton} from 'ng-simple-button'
 
@@ -10,5 +10,11 @@ import {NgSimpleButton} from 'ng-simple-button'
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PgButtons {
+  count = signal<number>(1);
 
+  testClick(event: MouseEvent){
+    const current = this.count();
+    this.count.set(current + 1);
+    console.log(event);
+  };
 }
--- projects/ng-simple-button/src/lib/ng-simple-button.ts
+++ projects/ng-simple-button/src/lib/ng-simple-button.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input, output } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -10,4 +10,65 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class NgSimpleButton {
 
+  // ----- ACTION INPUT
+  onClick = output<MouseEvent>();
+
+  handleClick(event: MouseEvent) {
+    this.onClick.emit(event);
+  };
+
+  // ----- ACCESSIBILITY INPUTS
+  ariaLabel = input<string | null>(null);
+  title = input<string | null>(null);
+  tabIndex = input<number | null>(null);
+  ariaCurrent = input<'page' | 'step' | 'true' | null>(null);
+  download = input<string | boolean | null>(null);
+  role = input<'link' | 'button' | null>(null);
+  disabled = input<boolean>(false);
+
+  // ----- STYLE INPUTS
+  type = input<'solid' | 'minimal' | 'outline'>('solid');
+  shape = input<'rounded' | 'square'>('rounded');
+  hover = input<'color' | 'scale' | 'outline'>('color');
+  direction = input<'row' | 'column'>('row');
+
+  // ----- HOST BINDINGS
+
+  // TYPE BINDINGS
+
+  @HostBinding('class.solid') get isSolid() {
+    return this.type() === 'solid';
+  };
+
+  @HostBinding('class.minimal') get isMinimal() {
+    return this.type() === 'minimal';
+  };
+
+  @HostBinding('class.outline') get isOutline() {
+    return this.type() === 'outline';
+  };
+
+  // SHAPE BINDINGS
+
+  @HostBinding('class.square') get isSquare() {
+    return this.shape() === 'square';
+  };
+
+  // HOVER BINDINGS
+
+  @HostBinding('class.colorHover') get isColorHover() {
+    return this.hover() === 'color';
+  };
+
+  @HostBinding('class.scaleHover') get isScaleHover() {
+    return this.hover() === 'scale';
+  };
+
+  @HostBinding('class.outlineHover') get isOutlineHover() {
+    return this.hover() === 'outline';
+  };
+
+  @HostBinding('class.disabled') get isDisabled() {
+    return this.disabled();
+  };
 }
