8e87f67178a7f2a769a8c3869548b4195d352bc6
aldaydev <aldaydev@gmail.com>
2025-11-11 19:01:36 +0100

    feat: logic styles and accessibility done + need to review #118

--- projects/demo-app/src/app/playgrounds/pg-scroll-nav/pg-scroll-nav.ts
+++ projects/demo-app/src/app/playgrounds/pg-scroll-nav/pg-scroll-nav.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgScrollNav } from 'ng-scroll-nav';
+import { NgLinkButton } from 'ng-link-button';
 
 @Component({
   selector: 'pg-scroll-nav',
-  imports: [NgScrollNav],
+  imports: [NgScrollNav, NgLinkButton],
   templateUrl: './pg-scroll-nav.html',
   styleUrl: './pg-scroll-nav.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
