05bb2e135fc7e5db58f87b0ec8a1a6d583b2a271
AldayDev <aldaydev@gmail.com>
2025-11-11 12:49:49 +0100

    revert: ng-menu-button and ng-image-card deleted from workspace #114 (#138)

--- projects/demo-app/src/app/app.ts
+++ projects/demo-app/src/app/app.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { PgMenuButton } from './playgrounds/pg-menu-button/pg-menu-button';
 import { PgThemeToggle } from "./playgrounds/pg-theme-toggle/pg-theme-toggle";
-import { NgMenuButton } from 'ng-menu-button';
 import { PgMenuToggle } from './playgrounds/pg-menu-toggle/pg-menu-toggle';
 import { PgHeroCarousel } from './playgrounds/pg-hero-carousel/pg-hero-carousel';
-import { PgImageCard } from "./playgrounds/pg-image-card/pg-image-card";
 import { PgContentCarousel } from "./playgrounds/pg-content-carousel/pg-content-carousel";
 import { PgButtons } from "./playgrounds/pg-buttons/pg-buttons";
 import { PgContentCard } from './playgrounds/pg-content-card/pg-content-card';
@@ -15,7 +12,6 @@ import { PgContentCard } from './playgrounds/pg-content-card/pg-content-card';
     PgMenuToggle,
     PgThemeToggle,
     PgHeroCarousel,
-    PgImageCard,
     PgContentCard,
     PgContentCarousel,
     PgButtons
--- projects/ng-image-card/src/public-api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of ng-image-card
- */
-
-export * from './lib/ng-image-card';
