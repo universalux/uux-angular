7451d45fb5ff8b7560983b7f06a4b8b139a07586
aldaydev <aldaydev@gmail.com>
2025-11-12 10:29:20 +0100

    feat: ng-scroll-nav logic and styles done #118

--- projects/demo-app/src/app/playgrounds/pg-scroll-nav/pg-scroll-nav.ts
+++ projects/demo-app/src/app/playgrounds/pg-scroll-nav/pg-scroll-nav.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NgScrollNav } from 'ng-scroll-nav';
-import { NgLinkButton } from 'ng-link-button';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { NgScrollNav, ScrollNavCustomAria } from 'ng-scroll-nav';
+import { NgLinkButton,  } from 'ng-link-button';
 
 @Component({
   selector: 'pg-scroll-nav',
@@ -10,5 +10,10 @@ import { NgLinkButton } from 'ng-link-button';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PgScrollNav {
-
+  accOpts = signal<ScrollNavCustomAria>({
+    navAriaLabel: 'EDITED Navigationsleiste',
+    prevBtnAriaLabel: 'EDITED Nach links scrollen',
+    nextBtnAriaLabel: 'EDITED Nach rechts scrollen',
+    linksGroupAriaLabel: 'EDITED Link-Gruppe',
+  });
 }
