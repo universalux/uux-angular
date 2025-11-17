80c8d4095df7e89eba50230091eeebb5c70cf7af
aldaydev <aldaydev@gmail.com>
2025-11-17 16:18:54 +0100

    feat: ng-expandable developed #149

--- projects/demo-app/src/app/playgrounds/pg-expandable/pg-expandable.ts
+++ projects/demo-app/src/app/playgrounds/pg-expandable/pg-expandable.ts
@@ -1,5 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ContentChildren, effect, QueryList, ViewChildren } from '@angular/core';
 import { NgExpandable } from 'ng-expandable';
+
+type ExpansionChange = {
+  state: boolean;
+  id: string;
+}
 
 @Component({
   selector: 'pg-expandable',
@@ -9,5 +14,30 @@ import { NgExpandable } from 'ng-expandable';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PgExpandable {
+
+  // handleExpansions({state, id}: ExpansionChange){
+  //   console.log(state, id);
+  // }
+
+  @ViewChildren('accordionItem') accordionItems!: QueryList<NgExpandable>;
+
+  ngAfterViewInit() {
+    console.log('Items encontrados:', this.accordionItems.length);
+    this.accordionItems.forEach((item, index) => {
+      item.isExpanded.subscribe((state) => {
+        if(state){
+          this.closeExpandables(index);
+        }
+      })
+    });
+  }
+
+  closeExpandables(openedExpandableIndex: number){
+    this.accordionItems.forEach((item, index) => {
+      if(index !== openedExpandableIndex){
+        item.isExpanded.set(false);
+      }
+    })
+  }
 
 }
--- projects/ng-expandable/src/lib/ng-expandable.ts
+++ projects/ng-expandable/src/lib/ng-expandable.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, ChangeDetectionStrategy, Component, effect, ElementRef, inject, input, model, OnInit, output, PLATFORM_ID, signal, ViewChild } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -8,8 +9,66 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   styleUrl: './ng-expandable.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NgExpandable {
+export class NgExpandable implements AfterViewInit, OnInit {
+
+  // ---- INJECTIONS
+
+  private isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+  private el = inject(ElementRef);
+
+  // ----- INPUTS
+
   label = input<string | null>(null);
-  expanded = input<boolean>(false);
+  startExpanded = input<boolean>(false);
+  iconType = input<'arrow'| 'plus'>('arrow');
+
+  tabIndex = input<number>(0);
+
+  // ----- SIGNALS
+
+  isExpanded = model<boolean>(false);
+  contentHeight = signal<number>(0);
+  expandableBtnId = signal<string>(this.generateUniqueId('expandable-btn'));
+  expandableContentId = signal<string>(this.generateUniqueId('expandable-content'));
+
+  // ----- VIEW CHILDREN
+
+  @ViewChild('content') content!: ElementRef;
+
+  // ----- LIFE CYCLE
+
+  ngAfterViewInit(): void {
+    if(this.isBrowser){
+      this.contentHeight.set(this.content.nativeElement.clientHeight);
+      this.handleSeparation();
+      requestAnimationFrame(() => {
+        this.el.nativeElement.classList.add('ready');
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    if(this.startExpanded()){
+      this.isExpanded.set(true);
+    }
+  }
+
+  // ----- METHODS
+
+  generateUniqueId(prefix = 'expandable-btn'): string {
+    const random = Math.random().toString(36).substring(2, 10);
+    return `${prefix}-${random}`;
+  }
+
+  handleExpansion(){
+    this.isExpanded.set(!this.isExpanded());
+    this.handleSeparation();
+  };
+
+  handleSeparation(){
+    this.isExpanded()
+      ? this.el.nativeElement.classList.add('separation')
+      : this.el.nativeElement.classList.remove('separation')
+  };
 
 }
