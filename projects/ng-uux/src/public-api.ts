52c189ebc6906ecde7bd23ea87d36a02da6adcfe
aldaydev <aldaydev@gmail.com>
2025-11-21 18:57:57 +0100

    deploy: ng-uux deployed #185

--- projects/ng-uux/src/public-api.ts
+++ projects/ng-uux/src/public-api.ts
@@ -4,30 +4,35 @@
 
 // ng-menu-toggle
 export * from 'ng-menu-toggle';
-export type { MenuToggleLangs, MenuToggleCustomAria } from 'ng-menu-toggle';
+export type { NgMenuToggle, MenuToggleLangs, MenuToggleCustomAria } from 'ng-menu-toggle';
 
 // ng-theme-toggle
 export * from 'ng-theme-toggle';
-export type { ThemeToggleLangs, ThemeToggleCustomAria } from 'ng-theme-toggle';
+export type { NgThemeToggle, ThemeToggleLangs, ThemeToggleCustomAria } from 'ng-theme-toggle';
 
 // ng-link-button & ng-simple-button
 export * from 'ng-simple-button';
 export * from 'ng-link-button';
 
+export type { NgLinkButton } from 'ng-link-button';
+export type { NgSimpleButton } from 'ng-simple-button';
+
 // ng-content-card
 export * from 'ng-content-card';
+export type { NgContentCard } from 'ng-content-card';
 
 // ng-content-carousel
 export * from 'ng-content-carousel';
-export type { ContentCarouselLangs, ContentCarouselCustomAria } from 'ng-content-carousel';
+export type { NgContentCarousel, ContentCarouselItemDirective, ContentCarouselLangs, ContentCarouselCustomAria } from 'ng-content-carousel';
 
 // ng-hero-carousel
 export * from 'ng-hero-carousel';
-export type { HeroCarouselItem, HeroCarouselLangs, HeroCarouselCustomAria } from 'ng-hero-carousel';
+export type { NgHeroCarousel, HeroCarouselItem, HeroCarouselLangs, HeroCarouselCustomAria } from 'ng-hero-carousel';
 
 // ng-scroll-nav
 export * from 'ng-scroll-nav';
-export type { ScrollNavLangs, ScrollNavCustomAria } from 'ng-scroll-nav';
+export type { NgScrollNav, ScrollNavLangs, ScrollNavCustomAria } from 'ng-scroll-nav';
 
 // ng-expand
 export * from 'ng-expand';
+export type { NgExpand } from 'ng-expand';
