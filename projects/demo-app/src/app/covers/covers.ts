a95d0894ad6651530444d47738a6ac0cc1bbbf16
aldaydev <aldaydev@gmail.com>
2025-11-28 19:38:32 +0100

    perf: og images created #213

---
+++ projects/demo-app/src/app/covers/covers.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-covers',
+  imports: [  ],
+  templateUrl: './covers.html',
+  styleUrl: './covers.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Covers {
+
+}
